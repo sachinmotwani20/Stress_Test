dfc724841cd0627d4d1aedd5913b6d01e17ebe93
Sachin Motwani <34841998+sachinmotwani20@users.noreply.github.com>
2023-05-25 17:28:02 +0530

    Create Stress_test.py

---
+++ Stress_test.py
@@ -0,0 +1,190 @@
+#Code to do GPU stress test
+#!/usr/bin/python
+
+import time
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import torch
+from tqdm import tqdm
+import torch.nn as nn
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def data_loader(data_dir,
+                batch_size,
+                random_seed=42,
+                valid_size=0.1,
+                shuffle=True,
+                test=False):
+  
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
+
+    # define transforms
+    transform = transforms.Compose([
+            transforms.Resize((227,227)),
+            transforms.ToTensor(),
+            normalize,
+    ])
+
+    if test:
+        dataset = datasets.CIFAR100(
+          root=data_dir, train=False,
+          download=True, transform=transform,
+        )
+
+        data_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, shuffle=shuffle
+        )
+
+        return data_loader
+
+    # load the dataset
+    train_dataset = datasets.CIFAR100(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
+
+    valid_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
+
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    if shuffle:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, sampler=train_sampler)
+ 
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, sampler=valid_sampler)
+
+    return (train_loader, valid_loader)
+
+
+# CIFAR100 dataset 
+train_loader, valid_loader = data_loader(data_dir='./data',
+                                         batch_size=64)
+
+test_loader = data_loader(data_dir='./data',
+                              batch_size=64,
+                              test=True)
+
+class VGG11(nn.Module):
+    def __init__(self, in_channels, num_classes=100):
+        super(VGG11, self).__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        # convolutional layers 
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(self.in_channels, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        # fully connected linear layers
+        self.linear_layers = nn.Sequential(
+            nn.Linear(in_features=512*7*7, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout2d(0.5),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout2d(0.5),
+            nn.Linear(in_features=4096, out_features=self.num_classes)
+        )
+    def forward(self, x):
+        x = self.conv_layers(x)
+        # flatten to prepare for the fully connected layers
+        x = x.view(x.size(0), -1)
+        x = self.linear_layers(x)
+        return x    
+
+num_classes = 100
+num_epochs = 500000
+batch_size = 16
+learning_rate = 0.005
+
+model = VGG11(3,num_classes).to(device)
+
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay = 0.005, momentum = 0.9)  
+
+
+# Train the model
+total_step = len(train_loader)
+
+total_step = len(train_loader)
+start1 = time.time()
+for epoch in range(num_epochs):
+    start = time.time()
+    for i, (images, labels) in enumerate(train_loader):  
+        # Move tensors to the configured device
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    end = time.time()
+    # print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+    #                .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+    # print('Time for this Epoch:', end-start)
+            
+    # Validation
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in valid_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+    
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total)) 
+end1 = time.time()
+print('Total Time taken:', end1-start1)
